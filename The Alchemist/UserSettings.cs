9ca9d0f87fce67e06fc65e8bc17a33d6d75d6373
Justin <Justin@DESKTOP-E3KQQ9L>
2016-10-12 22:54:47 +0200

    Added character type enumerator and added user settings class responsible for reading user settings from and writing user settings to an xml document.

---
+++ The Alchemist/UserSettings.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace The_Alchemist
+{
+    /*
+     * 
+     *  Responsible for reading and wrtiting to user-settings.xml.
+     *  As a result it will get and set the various user settings
+     *  
+     */
+    class UserSettings
+    {
+        /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+         *                         Attributes
+         * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+        private const string userSettingsFilename = "user-settings.xml";                                                        // The xml file to read user settings from and write settings to
+
+        private CharacterType mUserCharacterType;                                                                               // The type of character according to user settings
+        private int mCurrentLevel;                                                                                              // The level the user was last on before exitting
+        private int mHighestLevel;                                                                                              // The highest level the user has achieved    
+        private string mUsername;                                                                                               // The user's username        
+
+        /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+         *                          Functions
+         * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+        /*
+         * Initialises object by reading from the user settings xml file
+         */
+        UserSettings()
+        {
+            readSettings();
+        }
+
+        /*
+         * Write the settings to the xml file upon destruction of the object
+         */
+        ~UserSettings()
+        {
+            writeSettings();
+        }
+
+        /*
+         * Opens the file specified by userSettingsFilename, reads through
+         * the file and initialises the required attributes.
+         */
+        private void readSettings()
+        {
+            XmlTextReader reader = new XmlTextReader(userSettingsFilename);                                                     // Open the xml document
+
+            while (reader.Read())                                                                                               // Loop through nodes
+            {
+                switch (reader.Name)
+                {
+                    case "username":
+                        Username = reader.Value;
+                        break;
+
+                    case "character":
+                        UserCharacterType = getCharacterType(reader.Value);
+                        break;
+
+                    case "current-level":
+                        CurrentLevel = getLevel(reader.Value);
+                        break;
+
+                    case "highest-level":
+                        HighestLevel = getLevel(reader.Value);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+        /*
+         * Determines the character type based on input string
+         * and returns a valid character type
+         */
+        private CharacterType getCharacterType(string characterTypeString)
+        {
+            switch (characterTypeString)
+            {
+                case "Earth":
+                    return CharacterType.Earth;
+
+                case "Fire":
+                    return CharacterType.Fire;
+
+                case "Water":
+                    return CharacterType.Water;
+
+                case "Wind":
+                    return CharacterType.Wind;
+
+                default:
+                    return CharacterType.Earth;                                                                             // Return a default value if something went wrong
+            }
+        }
+
+        /*
+         * Converts a string to an int in the case of retrieving the user's
+         * current level and highest level from the xml file.
+         */
+        private int getLevel(string levelString)
+        {
+            try
+            {
+                return Convert.ToInt32(levelString);                                                                        // Attempt to convert the string to int and return it
+            }
+
+            catch(Exception)
+            {
+                return 1;                                                                                                   // If something went wrong return the default value of 1
+            }
+        }
+
+        /*
+         * Converts the character type passed as a parameter to a string.
+         */
+        private string placeCharacterType(CharacterType characterType)
+        {
+            switch (characterType)
+            {
+                case CharacterType.Earth:
+                    return "Earth";
+
+                case CharacterType.Fire:
+                    return "Fire";
+
+                case CharacterType.Water:
+                    return "Water";
+
+                case CharacterType.Wind:
+                    return "Wind";
+
+                default:
+                    return "Earth";                                                                                         // Return a default value if something went wrong
+            }
+        }
+        
+        /*
+         * Writes the settings currently in the object out to the xml file
+         * specified in the userSettingsFilename string.
+         */
+        private void writeSettings()
+        {
+            XmlDocument userSettingsDocument = new XmlDocument();                                                           // Create new xml document object
+            userSettingsDocument.Load(userSettingsFilename);                                                                // Load the document based on userSettingsFilename string
+
+            userSettingsDocument.SelectSingleNode("user-settings/username").InnerText = Username;
+            userSettingsDocument.SelectSingleNode("user-settings/character")
+                .InnerText = placeCharacterType(UserCharacterType);
+            userSettingsDocument.SelectSingleNode("user-settings/current-level").InnerText = CurrentLevel.ToString();
+            userSettingsDocument.SelectSingleNode("user-settings/highest-level").InnerText = CurrentLevel.ToString();
+
+            userSettingsDocument.Save(userSettingsFilename);                                                                // Save the user settings to the actual xml document
+        }
+
+        /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+         *                        Properties
+         * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+        public CharacterType UserCharacterType
+        {
+            get
+            {
+                return mUserCharacterType;
+            }
+
+            set
+            {
+                mUserCharacterType = value;
+            }
+        }
+
+        public int CurrentLevel
+        {
+            get
+            {
+                return mCurrentLevel;
+            }
+
+            set
+            {
+                mCurrentLevel = value;
+            }
+        }
+
+        public string Username
+        {
+            get
+            {
+                return mUsername;
+            }
+
+            set
+            {
+                mUsername = value;
+            }
+        }
+
+        public int HighestLevel
+        {
+            get
+            {
+                return mHighestLevel;
+            }
+
+            set
+            {
+                mHighestLevel = value;
+            }
+        }
+    }
+}
